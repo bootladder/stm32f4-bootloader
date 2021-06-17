3ad6f1cd4fd0d1d211019eca839f9bd754253568
Steve Anderson <steve@bootladder.com>
2021-06-16 23:15:15 -0400

    erase and write seem to work

--- src/main.c
+++ src/main.c
@@ -2,9 +2,37 @@
 #include "target/usart.h"
 #include "printing_help.h"
 
+#include "stm32f4xx_flash.h"
+
+#include <stdint.h>
+
+FLASH_Status status;
+
 void main(void){
     led_init();
     led_red_on();
     setup_usart();
     PRINTSTRING("STM32F4 BOOTLOADER\n")
+
+    FLASH_Unlock();
+
+    status = FLASH_EraseSector(FLASH_Sector_1, VoltageRange_3);
+
+    if(status == FLASH_COMPLETE)
+        PRINTSTRING("ERASE COMPLETE\n")
+    else
+        PRINTSTRING("ERASE  NOT COMPLETE\n")
+
+    status =
+            FLASH_ProgramWord(0x08004000, 0x12345678);
+
+    if(status == FLASH_COMPLETE)
+        PRINTSTRING("WRITE COMPLETE\n")
+    else
+        PRINTSTRING("WRITE NOT COMPLETE\n")
+
+
+//    FLASH_Lock();
+    PRINTSTRING("Done\n")
+
 }
