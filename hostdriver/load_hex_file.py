496286029b7c8ea87cd09ba8e0a041306bd02433
Steve Anderson <steve@bootladder.com>
2023-04-10 19:59:29 -0400

    load hex file

--- hostdriver/load_hex_file.py
+++ hostdriver/load_hex_file.py
@@ -6,6 +6,7 @@
 import sys
 import os
 import time
+from datetime import datetime #test
 
 filename = sys.argv[1]
 ttyfilename = sys.argv[2]
@@ -17,17 +18,46 @@ Lines = file1.readlines()
 count = 0
 # Strips the newline character
 
-with open(ttyfilename, 'r') as the_file:...
+with open(ttyfilename, 'r') as the_file:
     with open(ttyfilename, 'w') as writetty:
-9
+
+        print("Erasing sector 1:")
+        writetty.write("erase sector 1\n")
+        response = the_file.readline()
+
+        if response[0] == 'O':
+            print("Done..")
+        else:
+            print("FAIL ERASE 1\n")
+            sys.exit(1)
+
+        print("Erasing sector 2:")
+        writetty.write("erase sector 2\n")
+        response = the_file.readline()
+
+        if response[0] == 'O':
+            print("Done..")
+        else:
+            print("FAIL ERASE 2\n")
+            sys.exit(1)
+
         for line in Lines:
             count += 1
             print("Line{}: {}".format(count, line.strip()))
 
+            #print(line.encode('utf-8').hex())
+
             writetty.write(line)
 
+            t1 = datetime.now()
             response = the_file.readline()
-
-            if response[0] != 'Y':
+            t2 = datetime.now()
+            delta = t2 - t1
+            print("delta micros : " , (delta.microseconds))
+            print("len resp : " , len(response))
+            if response[0] != 'O':
                 print("BAD")
+                print("RESPONSE IS: " , response)
                 sys.exit(1)
+            
+            print("wrote")
